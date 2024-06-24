a0688edf057ee0b09e208fe3f610bd9b914df867
Adam Rashid <rashid.uj@gmail.com>
2024-06-24 22:42:10 +0800

    feat(pwa): use vite-pwa plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "LottieFiles Viewer",
+        short_name: "Viewer",
+        description: "A viewer for LottieFiles with offline support.",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "icons/vite.svg",
+            type: "image/svg+xml",
+            sizes: "192x192",
+          },
+          {
+            src: "icons/vite.svg",
+            type: "image/svg+xml",
+            sizes: "512x512",
+          },
+        ],
+        orientation: "portrait-primary",
+      },
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
